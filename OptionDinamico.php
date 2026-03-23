3442ee52d3c5557999bc4d7b36d6b6a9e175d9fd
sara-daw <sriverocaminero.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:11:53 +0100

    Fetch 'Jesuita' options from database
    Added SQL query to fetch 'Jesuita' from 'Alumnos' table.

--- OptionDinamico.php
+++ OptionDinamico.php
@@ -5,6 +5,8 @@
         return $conexion;
     }
     $conexion=conectar();
+    $sql = "SELECT Jesuita FROM Alumnos"; 
+    $resultado = $conexion->query($sql);
     while($fila = $resultado->fetch_array()){
         echo '<option value="' . $fila["Jesuita"] . '">' . $fila["Jesuita"] . '</option>';
     }
