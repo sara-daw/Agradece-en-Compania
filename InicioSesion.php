b859f79943accc6ecce7be901dace573e49fae8c
sara-daw <sriverocaminero.guadalupe@alumnado.fundacionloyola.net>
2026-03-25 10:26:06 +0100

    Refactor InicioSesion.php for session handling

--- InicioSesion.php
+++ InicioSesion.php
@@ -1,5 +1,8 @@
 <?php
     session_start();
+    if($_SESSION["IdAlu"] != null){
+        header("Location: CerrarSesion.php");
+    }
     function conectar(){
         $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BASEDEDATOS);
         $conexion->set_charset("utf8");
